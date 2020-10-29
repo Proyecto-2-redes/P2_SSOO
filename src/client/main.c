5771548efb6313d0b0168dcb42276be158be0d62
jpgonzalez4 <jpgonzalez4@uc.cl>
2020-10-29 12:52:58 -0300

    Add server base files

---
+++ src/client/main.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("hola, soy client\n");
+    return 0;
+}
---
+++ src/server/connection.h
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "communication.h"
+
+typedef struct players_info {
+    int socket_c1;
+    int socket_c2;
+    int socket_c3;
+    int socket_c4;
+    int socket_c5;
+    int socket_c6;
+    int socket_c7;
+    int socket_c8;
+} PlayersInfo;
+
+PlayersInfo* prepare_sockets_and_get_clients(char* IP, int port);
