7d53c6473601ae1f2a4d88ed5d0c7d4e812a3683
felipe-barria95 <febamo0@gmail.com>
2020-10-30 15:54:50 -0300

    fjhnsfsqk

--- src/server/main.c
+++ src/server/main.c
@@ -22,15 +22,6 @@ int main(int argc, char* argv[]) {
 
     pthread_create(&thread_id, NULL, prepare_sockets_and_get_clients, (void *)&args);
 
-    // char * welcome = "Bienvenido Cliente 1!!";
-    // server_send_message(players_info->socket_c1, 1, welcome);
-    //
-    //
-    // int sockets_array[8] = {players_info->socket_c1, players_info->socket_c2,
-    //                         players_info->socket_c3, players_info->socket_c4,
-    //                         players_info->socket_c5, players_info->socket_c6,
-    //                         players_info->socket_c7, players_info->socket_c8};
-    // int my_attention = 0;
     while (1){
       // Se obtiene el paquete del cliente 1
       //int msg_code = server_receive_id(sockets_array[my_attention]);
